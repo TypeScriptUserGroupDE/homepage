36e0c6f603ca21f3646240d2bfda5ad668361fa9
Christoph Strauß <tadeussenf@fastmail.fm>
2016-07-06 09:16:05 +0200

    remove sourcemaps

--- client/src/main.js
+++
@@ -1,8 +0,0 @@
-///<reference path="../typings/index.d.ts"/>
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_component_1 = require('./app.component');
-var core_1 = require('@angular/core');
-core_1.enableProdMode();
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
-//# sourceMappingURL=main.js.map
